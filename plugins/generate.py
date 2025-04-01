5e47db3ef103be63db6a99ce66ee3f64411209eb
Orcale oMii <oracle.omii@gmail.com>
2025-04-01 23:16:34 +0530

    Update generate.py

--- plugins/generate.py
+++ plugins/generate.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 import traceback
 from pyrogram.types import Message
@@ -84,6 +82,4 @@ async def main(bot: Client, message: Message):
     await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
 
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
